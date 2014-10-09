1f6dec71b4223e45665a45ffa90d2fc5c2f47846
jonsca <jonathans1978@hotmail.com>
2014-10-09 06:52:17 -0400

    Added a preliminary plot2.R file

---
+++ plot2.R
@@ -0,0 +1,16 @@
+plot2 = function(){
+  
+  library("data.table")
+  df<-fread('household_power_consumption.txt',sep=';',skip="1/2/2007",nrows=(60*24*2),data.table = FALSE)
+  
+  top<-fread('household_power_consumption.txt',header=TRUE,sep=';',nrows=0,na.strings='?')
+  
+  setnames(df,old=colnames(df),new=colnames(top))
+  
+  ##http://stackoverflow.com/questions/12303449/format-date-to-datetime-format-in-r
+
+  df$Date <- strptime(paste(df$Date, format(df$Time, nsmall=2)), format='%d/%m/%Y %H:%M:%S')
+  df <- df[,-2]
+  
+  plot(df[,1],df[,2])
+}
